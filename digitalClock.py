7d89a38b3a7550e4ef91e472d346540e8f78b09f
vishus9133 <vikas@vikass-MacBook-Pro.local>
2023-09-24 00:48:17 +0530

    added green background

--- digitalClock.py
+++ digitalClock.py
@@ -1,6 +1,11 @@
 from tkinter import Tk, Label
-window=Tk()
-window.title("Digital Clock")
+
+window = Tk()
+window.title("Digital CLock")
 window.geometry("600x300")
-window.mainloop()
-#adding a comment line
+window.configure(bg="darkgreen")
+
+label = Label(window, text="Hello!", font=("Arial Black",78,"bold"), bg="darkgreen", fg="white")
+label.pack(pady=100)
+
+window.mainloop() 
